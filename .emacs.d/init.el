322ee334b8fe5c42baf0a2d20955f7138ca11668
Zinzon <zinzon@zinzon.zinzon>
2018-02-04 20:59:28 -0500

    Added helm to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,7 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (slime evil ##))))
+ '(package-selected-packages (quote (helm auctex slime evil ##))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,3 +29,4 @@
 (require 'init-paredit)
 (require 'init-evil)
 (require 'init-slime)
+(require 'init-helm)
